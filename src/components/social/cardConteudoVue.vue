8f5685afdf979de50d12d2d7fcf99d7398b46ced
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-08-24 19:45:00 -0300

    Implementação do campo de publicação do feed

--- src/components/social/cardConteudoVue.vue
+++ src/components/social/cardConteudoVue.vue
@@ -1,30 +1,30 @@
 <template>
+    <div class="row">
+        <div class="card">
+            <div class="card-content">
+                <div class="row valign-wrapper">
+                    <gridVue tamanho="1">
+                        <img :src="img_perfil" alt="nome" class="circle responsive-img">
+                    </gridVue>
+                    <gridVue tamanho="11">
+                        <span class="black-text">
+                            <strong>{{ nome }}</strong> - <small>{{ data }}</small>
+                        </span>
+                    </gridVue>
+                </div>
 
-    <div class="card">
-        <div class="card-content">
-            <div class="row valign-wrapper">
-                <gridVue tamanho="1">
-                    <img :src="img_perfil" alt="nome" class="circle responsive-img">
-                </gridVue>
-                <gridVue tamanho="11">
-                    <span class="black-text">
-                        <strong>{{ nome }}</strong> - <small>{{ data }}</small>
-                    </span>
-                </gridVue>
             </div>
 
-        </div>
+            <slot />
 
-        <slot />
-
-        <div class="card-action">
-            <p>
-                <i class="material-icons">favorite_border</i>
-                <i class="material-icons">insert_comment</i>
-            </p>
+            <div class="card-action">
+                <p>
+                    <i class="material-icons">favorite_border</i>
+                    <i class="material-icons">insert_comment</i>
+                </p>
+            </div>
         </div>
     </div>
-
 </template>
 
 <script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,30 @@
 <template>
 
-  <div class="">
+  <span>
+
+    <div class="row">
+      <gridVue class="input-field" tamanho="12">
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+        <label for="textarea1">O que está acontecendo?</label>
+      </gridVue>
+
+      <p>
+        <gridVue class="btn waves-effect waves-light" tamanho="2 offset-s10" >Publicar</gridVue>
+      </p>
+    </div>
+
 
     <cardConteudoVue img_perfil="https://materializecss.com/images/yuna.jpg" nome="Matheus Cardoso"
       data="21/08/2024 20:30">
 
       <cardDetalheVue imagem="https://materializecss.com/images/sample-1.jpg" titulo="" descricao="I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively." />
+      I am convenient because I require little markup to use effectively." />
 
     </cardConteudoVue>
-  </div>
+
+  </span>
+
+
 
 
 </template>
@@ -17,6 +32,7 @@
 <script>
 import cardConteudoVue from '../components/social/cardConteudoVue.vue';
 import cardDetalheVue from '../components/social/cardDetalheVue.vue';
+import gridVue from '../components/layouts/gridVue.vue';
 
 export default {
   name: 'home',
@@ -27,7 +43,8 @@ export default {
   },
   components: {
     cardConteudoVue,
-    cardDetalheVue
+    cardDetalheVue,
+    gridVue
   }
 }
 </script>
